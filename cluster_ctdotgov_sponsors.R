60682b7c136961c75f37d5fc8a406951d615b1c5
Ben Berger <ben@Bens-MacBook-Pro.local>
2024-03-06 22:19:46 -0500

    We running this !

--- cluster_ctdotgov_sponsors.R
+++ cluster_ctdotgov_sponsors.R
@@ -1,8 +1,8 @@
 # --------------------------------------------------------------- #
-# 1. Clean ctdotgov firm names and cluster
+# Clean ctdotgov firm names and cluster
 # --------------------------------------------------------------- #
 library(tidyverse)
-library(haven)
+library(feather)
 
 # Text processing
 library(tm)
@@ -20,40 +20,28 @@ library(apcluster)
 # ------------------------------------ #
 # Load dictionary and cleaning function
 # ------------------------------------ #
-source("src/process_data/clean_sponsor_names.R")
+source("clean_sponsor_names.R")
 
-# --------------- #
-# Load data
-# --------------- #
+# ------------------------------------ #
 # Load clinicaltrials.gov sponsor data
-trial_sponsors <- readRDS("data/intermediate/trials/trial_sponsors.RDS")
-
-
-# Load drugs@fda data
-drugsatfda_applications <- read_tsv("data/raw/drugsatfda/2022-08-23/Applications.txt",
-                                    col_types = "cccc") 
-
-drugsatfda_submissions <- suppressWarnings(
-  read_tsv("data/raw/drugsatfda/2022-08-23/Submissions.txt",
-           col_types = "cicicTcc"))
-
+# ------------------------------------ #
+trial_sponsors <- read_feather("data/ctdotgov/sponsors.feather")
 
 # --------------------------------------------------------------- #
-# 1. Pre-process initial cleaning step
+# 1. Clean firm names and create corpora
 # --------------------------------------------------------------- #
-
 # Create crosswalk between ctdotgov names and corpus names
 ctdotgov_names <- trial_sponsors %>% 
-  # Keep firms (drop universities, governments, other non-private entities)
-  filter(lead_or_collaborator == "lead" & agency_class == "INDUSTRY") %>% 
-  # Look-up corpus name if different from actual name
-  mutate(ctdotgov_name = name,
-         corpus_name = coalesce(firm_dictionary[ctdotgov_name], ctdotgov_name)) %>% 
-  # Count trials per firm name
-  group_by(ctdotgov_name, corpus_name) %>% 
-  summarize(n_trials = n(),
-            .groups = "drop")
-  
+	# Keep firms (drop universities, governments, other non-private entities)
+	filter(lead_or_collaborator == "lead" & agency_class == "INDUSTRY") %>% 
+	# Look-up corpus name if different from actual name
+	mutate(ctdotgov_name = name,
+				 corpus_name = coalesce(firm_dictionary[ctdotgov_name], ctdotgov_name)) %>% 
+	# Count trials per firm name
+	group_by(ctdotgov_name, corpus_name) %>% 
+	summarize(n_trials = n(),
+						.groups = "drop")
+
 # Create vector of unique corpus names 
 corpus_names <- sort(unique(ctdotgov_names$corpus_name))
 
@@ -65,10 +53,10 @@ ctdotgov_corpus_B <- clean_sponsor_names(ctdotgov_corpus_A)
 
 # Create document term matrix, weighting for term frequency
 dtm <- ctdotgov_corpus_B %>% 
-  DocumentTermMatrix(list(wordLengths = c(1, Inf))) %>%
-  weightTfIdf() %>%
-  tidy() %>% 
-  cast_sparse(row = document, column = term, value = count, repr = "T")
+	DocumentTermMatrix(list(wordLengths = c(1, Inf))) %>%
+	weightTfIdf() %>%
+	tidy() %>% 
+	cast_sparse(row = document, column = term, value = count, repr = "T")
 
 # Print head and tail of corpus
 inspect(head(ctdotgov_corpus_B, 20))
@@ -85,33 +73,33 @@ stopifnot(min(nchar(ctdotgov_corpus_B$content)) > 0)
 simils <- proxyC::simil(dtm, method = "cosine")
 
 # Run affinity propogation clustering on subset of data
-N <- nrow(simils) 
+N <- nrow(simils)
 s <- simils[1:N, 1:N]
 system.time(apres <- apcluster(s, lam = 0.5, q = 0.99995, seed = 1))
 
 # Link exemplars to terms
 exemplars <- tibble(
-  firm_id = as.integer(rownames(s)),
-  row = 1:N,
-  exrow = apres@idx,
-  name = corpus_names[firm_id],
-  exemplar_id = firm_id[exrow],
-  exemplar_name = name[exrow]
+	firm_id = as.integer(rownames(s)),
+	row = 1:N,
+	exrow = apres@idx,
+	name = corpus_names[firm_id],
+	exemplar_id = firm_id[exrow],
+	exemplar_name = name[exrow]
 ) 
 
 # Create unique firm IDs
 firm_xwalk <- ctdotgov_names %>%
-  left_join(exemplars, by = c("corpus_name" = "name")) %>% 
-  # Re-select exemplars according to firm name with most trials
-  group_by(exemplar_id) %>% 
-  mutate(new_exemplar_name = ctdotgov_name[n_trials == max(n_trials)][1]) %>% 
-  ungroup() %>% 
-  select(ctdotgov_name, corpus_name, firm_id = exemplar_id, firm_name = new_exemplar_name, n_trials)
+	left_join(exemplars, by = c("corpus_name" = "name")) %>% 
+	# Re-select exemplars according to firm name with most trials
+	group_by(exemplar_id) %>% 
+	mutate(new_exemplar_name = ctdotgov_name[n_trials == max(n_trials)][1]) %>% 
+	ungroup() %>% 
+	select(ctdotgov_name, corpus_name, firm_id = exemplar_id, firm_name = new_exemplar_name, n_trials)
 
 # ---------- #
 # Save data
 # ---------- #
-saveRDS(firm_xwalk, "data/intermediate/xwalks/firm_xwalk.RDS")
+write_feather(firm_xwalk, "firm_xwalk.feather")
 
 
 
--- join_drugsatfda_approvals.R
+++ join_drugsatfda_approvals.R
@@ -1,33 +1,34 @@
 # --------------------------------------------------------------- #
 # Join Drugs@FDA approved drugs to trial sponsors
 # --------------------------------------------------------------- #
-
 library(tidyverse)
-library(lubridate)
+library(feather)
+
+# Text processing
 library(tm)
-library(fuzzyjoin)
+
+# Diagnostics
 library(gtsummary)
 
 
 # ---------------------- #
 # Load cleaning function
 # ---------------------- #
-source("src/process_data/clean_sponsor_names.R")
+source("clean_sponsor_names.R")
 
 # --------------- #
 # Load data
 # --------------- #
 # Load trial sponsor to firm id crosswalk
-firm_xwalk <- readRDS("data/intermediate/xwalks/firm_xwalk.RDS")
+firm_xwalk <- read_feather("firm_xwalk.feather")
 
 # Load Drugs@FDA sponsors
-drugsatfda_applications <- read_tsv("data/raw/drugsatfda/2022-08-23/Applications.txt",
-                                    col_types = "cccc") 
+drugsatfda_applications <- read_tsv("data/drugsatfda/Applications.txt",
+																		col_types = "cccc") 
 
 # Load Drugs@FDA application submission info
-drugsatfda_submissions <- suppressWarnings(
-  read_tsv("data/raw/drugsatfda/2022-08-23/Submissions.txt",
-           col_types = "cicicTcc"))
+drugsatfda_submissions <- suppressWarnings(read_tsv("data/drugsatfda/Submissions.txt",
+																										col_types = "cicicTcc"))
 
 # ------------------------------------- #
 # Clean trial and drug sponsors
@@ -51,19 +52,19 @@ length(ctdotgov_corpus_names) + length(drugsatfda_corpus_names)
 # ------------------------------------- #
 # Create document term matrix with all rows
 full_dtm <- corpus_B %>% 
-  DocumentTermMatrix(list(wordLengths = c(1, Inf))) 
+	DocumentTermMatrix(list(wordLengths = c(1, Inf))) 
 
 # Select subset of rows for ctdotgov firms
 ctdotgov_dtm <- full_dtm %>% 
-  head(length(ctdotgov_corpus_names)) %>% 
-  weightTfIdf() %>%
-  as.matrix()
+	head(length(ctdotgov_corpus_names)) %>% 
+	weightTfIdf() %>%
+	as.matrix()
 
 # Select subset of rows for drugsatfda firms
 drugsatfda_dtm <- full_dtm %>% 
-  tail(length(drugsatfda_corpus_names)) %>%
-  weightTfIdf() %>%
-  as.matrix()
+	tail(length(drugsatfda_corpus_names)) %>%
+	weightTfIdf() %>%
+	as.matrix()
 
 # ------------------------------------- #
 # Link trial sponsors to drug sponsors
@@ -72,56 +73,56 @@ simils <- proxyC::simil(ctdotgov_dtm, drugsatfda_dtm, method = "cosine")
 
 # For each ctdotgov sponsor, find Drugs@FDA sponsor that is most similar
 matches <- tibble(ctdotgov_corpus_name = ctdotgov_corpus_names,
-                  match_id = apply(simils, 1, which.max),
-                  similarity = apply(simils, 1, max),
-                  drugsatfda_name = drugsatfda_corpus_names[match_id]) 
+									match_id = apply(simils, 1, which.max),
+									similarity = apply(simils, 1, max),
+									drugsatfda_name = drugsatfda_corpus_names[match_id]) 
 
 
 # Filter out bad matches
 good_matches <- matches %>% 
-  filter(similarity >= 0.75) %>% 
-  select(-match_id)
+	filter(similarity >= 0.75) %>% 
+	select(-match_id)
 
 # Join with rest of data
 matched_firms <- firm_xwalk %>% 
-  left_join(good_matches, c("corpus_name" = "ctdotgov_corpus_name")) %>% 
-  select(firm_id, firm_name, ctdotgov_name, drugsatfda_name, n_trials) 
+	left_join(good_matches, c("corpus_name" = "ctdotgov_corpus_name")) %>% 
+	select(firm_id, firm_name, ctdotgov_name, drugsatfda_name, n_trials) 
 
 
 # ------------------------------------------------ #
 # Get drug approval dates
 # ------------------------------------------------ #
 approval_dates <- drugsatfda_submissions %>%
-  filter(SubmissionType == "ORIG") %>%
-  group_by(ApplNo) %>%
-  summarize(approval_date = as_date(min(SubmissionStatusDate)))
+	filter(SubmissionType == "ORIG") %>%
+	group_by(ApplNo) %>%
+	summarize(approval_date = as_date(min(SubmissionStatusDate)))
 
 
 # ------------------------------------------------------------ #
 # Get first approval date for each Drugs@FDA sponsor
 # ------------------------------------------------------------ #
 drugsatfda_first_approvals <- drugsatfda_applications %>%
-  # Join approval dates
-  inner_join(approval_dates, by = "ApplNo") %>%
-  rename(drugsatfda_name = SponsorName) %>%
-  # Get date of first approval for each sponsor
-  group_by(drugsatfda_name) %>% 
-  summarize(n_approvals = n(),
-            first_approval_date = min(approval_date),
-            .groups = "drop")
+	# Join approval dates
+	inner_join(approval_dates, by = "ApplNo") %>%
+	rename(drugsatfda_name = SponsorName) %>%
+	# Get date of first approval for each sponsor
+	group_by(drugsatfda_name) %>% 
+	summarize(n_approvals = n(),
+						first_approval_date = min(approval_date),
+						.groups = "drop")
 
 
 # ------------------------------------------------------------ #
 # Aggregate approvals to the firm level
 # ------------------------------------------------------------ #
 drug_approval_dates <- matched_firms %>%
-  left_join(drugsatfda_first_approvals, by = "drugsatfda_name") %>%
-  # Aggregate to firm level
-  group_by(firm_id, firm_name) %>%
-  summarize(n_trials = sum(n_trials),
-            first_approval_date = min(replace_na(first_approval_date, as_date(Inf))),
-            n_approvals = sum(replace_na(n_approvals, 0)),
-            .groups = "drop") 
+	left_join(drugsatfda_first_approvals, by = "drugsatfda_name") %>%
+	# Aggregate to firm level
+	group_by(firm_id, firm_name) %>%
+	summarize(n_trials = sum(n_trials),
+						first_approval_date = min(replace_na(first_approval_date, as_date(Inf))),
+						n_approvals = sum(replace_na(n_approvals, 0)),
+						.groups = "drop") 
 
 
 # ------------------------------------------------------------ #
@@ -129,15 +130,15 @@ drug_approval_dates <- matched_firms %>%
 # ------------------------------------------------------------ #
 # Check that we get a decent number of matches
 drug_approval_dates %>% 
-  transmute("Total Trials" = n_trials,
-            "Total Approvals" = n_approvals,
-            "Any Approval" = first_approval_date < Inf) %>% 
-  tbl_summary(
-    statistic = list(all_continuous() ~ "{mean} ({sd})", 
-                     all_categorical() ~ "{n} ({p}%)"),
-    digits = ~ c(1, 1)) %>% 
-  as_tibble() %>% 
-  print()
+	transmute("Total Trials" = n_trials,
+						"Total Approvals" = n_approvals,
+						"Any Approval" = first_approval_date < Inf) %>% 
+	tbl_summary(
+		statistic = list(all_continuous() ~ "{mean} ({sd})", 
+										 all_categorical() ~ "{n} ({p}%)"),
+		digits = ~ c(1, 1)) %>% 
+	as_tibble() %>% 
+	print()
 
 # Check that more trials associated with more approvals
 print(summary(lm(n_approvals ~ n_trials, drug_approval_dates)))
@@ -146,7 +147,7 @@ print(summary(lm(n_approvals ~ n_trials, drug_approval_dates)))
 # ---------- #
 # Save data
 # ---------- #
-saveRDS(drug_approval_dates, "data/intermediate/drug_approval_dates.RDS")
+write_feather(drug_approval_dates, "drug_approval_dates.feather")
 
 
 
